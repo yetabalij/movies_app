30953336d72e030f168efbbd019ce0e80075f27a
yetabalij <yetabalij@gmail.com>
2021-03-22 10:37:32 +0300

    server app is initiated in appmjs file, database connection is established

---
+++ server/app.js
@@ -0,0 +1,34 @@
+//imports
+require("dotenv").config();
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
+
+const app = express();
+const port = process.env.port || 5000;
+
+//middlewares
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static("uploads"));
+
+//database connection
+mongoose
+  .connect(process.env.DB_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useFindAndModify: true,
+    useCreateIndex: true,
+  })
+  .then(() => {
+    console.log("Database connected successfully");
+  })
+  .catch((err) => {
+    console.log(err);
+  });
+
+//start server
+app.listen(port, () => {
+  console.log(`server running at localhost:${port}`);
+});
