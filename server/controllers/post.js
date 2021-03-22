903fdc13f0ead136fbf3a3e4022da45258504d77
yetabalij <yetabalij@gmail.com>
2021-03-22 15:58:18 +0300

    all the routes in the controller file and routes file are organized

--- server/controllers/post.js
+++ server/controllers/post.js
@@ -1,4 +1,26 @@
+const { post } = require("../routes/routes");
+
 //fetch all post controller
 exports.fetchAllPost = async (req, res) => {
-  res.send("Hello Ermias Tsegu");
+  res.send("fetch all post");
+};
+
+//fetch by Id
+exports.fetchSinglePost = async (req, res) => {
+  res.send("fetch single post");
+};
+
+//create a post
+exports.createPost = async (req, res) => {
+  res.send("create a post");
+};
+
+//update post
+exports.updatePost = async (req, res) => {
+  res.send("update post");
+};
+
+//delete a post
+exports.deletePost = async (req, res) => {
+  res.send("delete post");
 };
--- server/routes/routes.js
+++ server/routes/routes.js
@@ -1,8 +1,18 @@
 const express = require("express");
-const { fetchAllPost } = require("./../controllers/post");
+const {
+  fetchAllPost,
+  fetchSinglePost,
+  createPost,
+  updatePost,
+  deletePost,
+} = require("./../controllers/post");
 
 const router = express.Router();
 
-router.get("/", fetchAllPost);
+router.get("/fetchallpost", fetchAllPost);
+router.get("/fetchsinglepost:id", fetchSinglePost);
+router.post("/createpost", createPost);
+router.patch("/updatepost:id", updatePost);
+router.delete("/deletepost:id", deletePost);
 
 module.exports = router;
